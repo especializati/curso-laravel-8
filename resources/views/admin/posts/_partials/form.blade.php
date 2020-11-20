d94de184d09de014ed676dcb6abe652d12042a51
Carlos Ferreira <conta@especializati.com.br>
2020-11-20 11:04:47 -0300

    Reuse Components Blade

---
+++ resources/views/admin/posts/_partials/form.blade.php
@@ -0,0 +1,12 @@
+@if ($errors->any())
+    <ul>
+        @foreach ($errors->all() as $error)
+            <li>{{ $error }}</li>
+        @endforeach
+    </ul>
+@endif
+
+@csrf
+<input type="text" name="title" id="title" placeholder="Título" value="{{ $post->title ?? old('title') }}">
+<textarea name="content" id="content" cols="30" rows="4" placeholder="Conteúdo">{{ $post->content ?? old('content') }}</textarea>
+<button type="submit">Enviar</button>
--- resources/views/admin/posts/create.blade.php
+++ resources/views/admin/posts/create.blade.php
@@ -1,16 +1,5 @@
 <h1>Cadastrar Novo Post</h1>
 
-@if ($errors->any())
-    <ul>
-        @foreach ($errors->all() as $error)
-            <li>{{ $error }}</li>
-        @endforeach
-    </ul>
-@endif
-
 <form action="{{ route('posts.store') }}" method="post">
-    @csrf
-    <input type="text" name="title" id="title" placeholder="Título" value="{{ old('title') }}">
-    <textarea name="content" id="content" cols="30" rows="4" placeholder="Conteúdo">{{ old('content') }}</textarea>
-    <button type="submit">Enviar</button>
+    @include('admin.posts._partials.form')
 </form>
