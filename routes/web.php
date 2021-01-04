31f28590a8b1c368781a25e6787b009129405d90
Carlos Ferreira <conta@especializati.com.br>
2021-01-04 15:18:15 -0300

    Apply Middleware Auth Routes

--- routes/web.php
+++ routes/web.php
@@ -5,14 +5,16 @@ use App\Http\Controllers\{
 };
 use Illuminate\Support\Facades\Route;
 
-Route::any('/posts/search', [PostController::class, 'search'])->name('posts.search');
-Route::get('/posts/create', [PostController::class, 'create'])->name('posts.create');
-Route::put('/posts/{id}', [PostController::class, 'update'])->name('posts.update');
-Route::get('/posts/edit/{id}', [PostController::class, 'edit'])->name('posts.edit');
-Route::delete('/posts/{id}', [PostController::class, 'destroy'])->name('posts.destroy');
-Route::get('/posts/{id}', [PostController::class, 'show'])->name('posts.show');
-Route::post('/posts', [PostController::class, 'store'])->name('posts.store');
-Route::get('/posts', [PostController::class, 'index'])->name('posts.index');
+Route::middleware(['auth'])->group(function () {
+    Route::any('/posts/search', [PostController::class, 'search'])->name('posts.search');
+    Route::get('/posts/create', [PostController::class, 'create'])->name('posts.create');
+    Route::put('/posts/{id}', [PostController::class, 'update'])->name('posts.update');
+    Route::get('/posts/edit/{id}', [PostController::class, 'edit'])->name('posts.edit');
+    Route::delete('/posts/{id}', [PostController::class, 'destroy'])->name('posts.destroy');
+    Route::get('/posts/{id}', [PostController::class, 'show'])->name('posts.show');
+    Route::post('/posts', [PostController::class, 'store'])->name('posts.store');
+    Route::get('/posts', [PostController::class, 'index'])->name('posts.index');
+});
 
 Route::get('/dashboard', function () {
     return view('dashboard');
