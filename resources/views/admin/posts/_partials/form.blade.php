72fe1fc6ff0493bd265f1e323413a925223c921f
Rafael-devops <rafaelllcomputadores892@gmail.com>
2022-01-27 06:08:42 -0300

    alterações na view

--- resources/views/admin/posts/_partials/form.blade.php
+++ resources/views/admin/posts/_partials/form.blade.php
@@ -10,4 +10,4 @@
 <input type="file" name="image" id="image" class="block w-full p-3 mt-2 text-gray-700 bg-gray-200 appearance-none focus:outline-none focus:bg-gray-300 focus:shadow-inner">
 <input type="text" name="title" id="title" placeholder="Título" value="{{ $post->title ?? old('title') }}" class="block w-full p-3 mt-2 text-gray-700 bg-gray-200 appearance-none focus:outline-none focus:bg-gray-300 focus:shadow-inner">
 <textarea name="content" id="content" cols="30" rows="4" placeholder="Conteúdo" class="block w-full p-3 mt-2 text-gray-700 bg-gray-200 appearance-none focus:outline-none focus:bg-gray-300 focus:shadow-inner">{{ $post->content ?? old('content') }}</textarea>
-<button type="submit" class="w-full py-3 mt-6 font-medium tracking-widest text-white uppercase bg-black shadow-lg focus:outline-none hover:bg-gray-900 hover:shadow-none">Enviar</button>
+<button type="submit" class="w-full py-3 mt-6 font-medium tracking-widest text-white uppercase bg-black shadow-lg focus:outline-none hover:bg-gray-900 hover:shadow-none"><i class="fas fa-paper-plane"></i>Enviar</button>
--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -7,7 +7,7 @@
 <h1 class="text-center text-3xl uppercase font-black my-4">Listagem dos Posts</h1>
 
 <div class="grid">
-    <a href="{{ route('posts.create') }}" class="my-4 uppercase px-8 py-2 rounded bg-green-600 text-blue-50 max-w-max shadow-sm hover:shadow-lg">Criar Novo Post</a>
+    <a href="{{ route('posts.create') }}" class="my-4 uppercase px-8 py-2 rounded bg-green-600 text-blue-50 max-w-max shadow-sm hover:shadow-lg"><i class="fas fa-plus-circle"></i></i>Criar Novo Post</a>
 </div>
 @if (session('message'))
     <div class="flex justify-center items-center m-1 font-medium py-1 px-2 bg-white rounded-md text-green-700 bg-green-100 border border-green-300 ">
@@ -19,7 +19,7 @@
     @csrf
     <div class="max-w-sm my-4 p-1 pr-0 flex items-center">
         <input type="text" name="search" placeholder="Filtrar:" class="flex-1 appearance-none rounded shadow p-3 text-grey-dark mr-2 focus:outline-none">
-        <button type="submit" class="uppercase p-3 rounded bg-blue-500 text-blue-50 max-w-max shadow-sm hover:shadow-lg">Filtrar</button>
+        <button type="submit" class="uppercase p-3 rounded bg-blue-500 text-blue-50 max-w-max shadow-sm hover:shadow-lg"><i class="fas fa-search"></i>Filtrar</button>
     </div>
 </form>
 
@@ -41,10 +41,11 @@
                 <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-500">
                     <img src="{{ url("storage/{$post->image}") }}" alt="{{ $post->title }}" class="w-16">
                 </td>
+                
                 <td class="px-6 py-4 whitespace-no-wrap border-b text-blue-900 border-gray-500 text-sm leading-5">{{ $post->title }}</td>
                 <td class="px-6 py-4 whitespace-no-wrap border-b text-blue-900 border-gray-500 text-sm leading-5 text-right">
-                    <a href="{{ route('posts.show', $post->id) }}" class="px-5 py-2 border-blue-500 border text-blue-500 rounded transition duration-300 hover:bg-blue-700 hover:text-white focus:outline-none">Ver</a>
-                    <a href="{{ route('posts.edit', $post->id) }}" class="px-5 py-2 border-green-500 border text-green-500 rounded transition duration-300 hover:bg-green-700 hover:text-white focus:outline-none">Edit</a>
+                    <a href="{{ route('posts.show', $post->id) }}" class="px-5 py-2 border-blue-500 border text-blue-500 rounded transition duration-300 hover:bg-blue-700 hover:text-white focus:outline-none"><i class="fas fa-eye"></i>Ver</a>
+                    <a href="{{ route('posts.edit', $post->id) }}" class="px-5 py-2 border-green-500 border text-green-500 rounded transition duration-300 hover:bg-green-700 hover:text-white focus:outline-none"><i class="fas fa-edit"></i></i>Editar</a>
                 </td>
             </tr>
         @endforeach
